7aa2508773567fff13e03c8efbfa525ee80a3c84
Daniel Ranchal Parrado <danielranchal@correo.ugr.es>
2022-04-13 17:58:21 +0200

    Modify test

--- assignment2/test_and_plots.R
+++ assignment2/test_and_plots.R
@@ -132,12 +132,18 @@ wilcox.test(
     paired = TRUE
 )
 
+median(hoeffding.NoPrequentialVsPrequential$`Hoeffding sin prequential`)
+median(hoeffding.NoPrequentialVsPrequential$`Hoeffding con prequential`)
+
 wilcox.test(
     hoeffding.adaptativo.NoPrequentialVsPrequential$`Hoeffding adaptativo sin prequential`,
     hoeffding.adaptativo.NoPrequentialVsPrequential$`Hoeffding adaptativo con prequential`,
     alternative = "two.sided",
     paired = TRUE
 )
+
+median(hoeffding.adaptativo.NoPrequentialVsPrequential$`Hoeffding adaptativo sin prequential`)
+median(hoeffding.adaptativo.NoPrequentialVsPrequential$`Hoeffding adaptativo con prequential`)
 
 hoeffding.NoPrequentialVsPrequential %>% gather(
     "Variables",
@@ -171,34 +177,52 @@ hoeffding.adaptativo.NoPrequentialVsPrequential %>% gather(
 single_classifier_drift_hoeffding_online_drift <- scan("single_classifier_drift_hoeffding_online_drift.txt")
 single_classifier_drift_hoeffding_adaptativo_online_drift <- scan("single_classifier_drift_hoeffding_adaptativo_online_drift.txt")
 
-single_classifier_drift_online_drift <- data.frame(
-    "Hoeffding"=single_classifier_drift_hoeffding_online_drift,
-    "Hoeffding adaptativo"=single_classifier_drift_hoeffding_adaptativo_online_drift,
+hoeffding.single_classifier_driftVsNosingle_classifier_drift <- data.frame(
+    "Hoeffding sin reinicio"=hoeffding_online_drift,
+    "Hoeffding con reinicio"=single_classifier_drift_hoeffding_online_drift,
     check.names = F
 )
 
-diffs <- (single_classifier_drift_online_drift$Hoeffding - single_classifier_drift_online_drift$`Hoeffding adaptativo`) / single_classifier_drift_online_drift$Hoeffding
-wilcoxon <- cbind(
-    ifelse(
-        diffs<0,
-        abs(diffs)+0.1,
-        0+0.1
-    ),
-    ifelse(
-        diffs>0,
-        abs(diffs)+0.1,
-        0+0.1
-    )
+hoeffding.adaptativo.single_classifier_driftVsNosingle_classifier_drift <- data.frame(
+    "Hoeffding adaptativo sin reinicio"=hoeffding_adaptativo_online_drift,
+    "Hoeffding adaptativo con reinicio"=single_classifier_drift_hoeffding_adaptativo_online_drift,
+    check.names = F
 )
 
 wilcox.test(
-    wilcoxon[, 1],
-    wilcoxon[, 2],
+    hoeffding.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding sin reinicio`,
+    hoeffding.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding con reinicio`,
     alternative = "two.sided",
     paired = TRUE
 )
 
-single_classifier_drift_online_drift %>% gather(
+median(hoeffding.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding sin reinicio`)
+median(hoeffding.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding con reinicio`)
+
+wilcox.test(
+    hoeffding.adaptativo.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding adaptativo sin reinicio`,
+    hoeffding.adaptativo.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding adaptativo con reinicio`,
+    alternative = "two.sided",
+    paired = TRUE
+)
+
+median(hoeffding.adaptativo.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding adaptativo sin reinicio`)
+median(hoeffding.adaptativo.single_classifier_driftVsNosingle_classifier_drift$`Hoeffding adaptativo con reinicio`)
+
+hoeffding.single_classifier_driftVsNosingle_classifier_drift %>% gather(
+    "Variables",
+    "Values"
+) %>% ggplot(
+    .,
+    aes(y=Values)
+) + geom_boxplot() + facet_wrap(
+    ~ Variables
+) + labs(
+    title="Entrenamiento online en datos con concept drift. Reinicialización",
+    y="Porcentaje de acierto"
+)
+
+hoeffding.adaptativo.single_classifier_driftVsNosingle_classifier_drift %>% gather(
     "Variables",
     "Values"
 ) %>% ggplot(
